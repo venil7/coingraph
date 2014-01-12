39264fb5f1a9f5f726227c4e836e8098d20b35a0
Art <github@darkruby.co.uk>
2014-01-12 13:38:06 +0000

    added readme;using new node-static pattern;removed config.json from repo;updated webapp.conf

--- index.js
+++ index.js
@@ -6,12 +6,16 @@ var static = require('node-static'),
     config = require('./config');
 
 var logger = Logger(module, config.logging.web);
-var staticServer = new static.Server(config.web);
+var staticServer = new static.Server(config.web, { cache: 7200 });
 
 var http = http.createServer(function (request, response) {
-  request.addListener('end', function () {
-    staticServer.serve(request, response);
-  }).resume();
+  staticServer.serve(request, response, function (err, res) {
+    if (err) {
+      logger.error('error serving', request.url, err.message);
+      response.writeHead(err.status, err.headers);
+      response.end();
+    }
+  });
 }).listen(process.env.PORT || 3000);
 
 var io = socket.listen(http, { log: false });
