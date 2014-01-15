be9ca838a1579b386120ba31208de3081368ec0d
Art <github@darkruby.co.uk>
2014-01-15 20:59:49 +0000

    added config settings

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var static = require('node-static'),
     config = require('./config');
 
 var logger = Logger(module, config.logging.web);
-var staticServer = new static.Server(config.web, { cache: 7200 });
+var staticServer = new static.Server(config.web.public, { cache: config.web.cache || 7200 });
 
 var http = http.createServer(function (request, response) {
   staticServer.serve(request, response, function (err, res) {
@@ -16,7 +16,7 @@ var http = http.createServer(function (request, response) {
       response.end();
     }
   });
-}).listen(process.env.PORT || 3000);
+}).listen(config.web.port || 3000, config.web.host || "localhost");
 
 var io = socket.listen(http, { log: false });
 var query = new Query.Query(config);
